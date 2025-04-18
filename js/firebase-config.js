d7037f84fbae92cda562202b185352958e13f09b
rhahavy <rhahavy.b@gmail.com>
2025-04-18 08:25:26 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIlkCuaWm1YkomfGape6zl2z7aJrRzwJw",
+   apiKey: "AIzaSyAIlkCuaWm1YkomfGape6zl2z7aJrRzwJw",
   authDomain: "eschool-gradebook.firebaseapp.com",
   projectId: "eschool-gradebook",
-  storageBucket: "eschool-gradebook.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "eschool-gradebook.firebasestorage.app",
+  messagingSenderId: "37242341415",
+  appId: "1:37242341415:web:5cc1863e6615874d84a602",
+  measurementId: "G-CRTK5FD726"
 };
 
 const app = initializeApp(firebaseConfig);
